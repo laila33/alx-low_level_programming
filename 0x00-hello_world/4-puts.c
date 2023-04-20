b602754f9254c4bb295134396862c806aa62fbce
laila33 <lailaabdelnaser64@gmail.com>
2023-04-20 04:47:36 -0700

    co

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point 
+ * Description - A C program to print with put function
+ * Return - always 0 (success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
