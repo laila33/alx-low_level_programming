5043547bfbda43deee2578886a475293f2a0dcd9
laila33 <lailaabdelnaser64@gmail.com>
2023-04-27 14:43:10 -0700

    a'

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,29 @@
  */
 int main(void)
 {
-int n;
-for (n = 1; n <= 100; n++)
-{
-if ((n % 3) == 0 && n % 5 == 0)
-printf("FizzBuzz");
-else if ((n % 3) == 0)
-printf("Fizz");
-else if ((n % 5) == 0)
-print("Buzz");
-else
-printf("%d", n);
-if (n == 100)
-continue;
-printf(" ");
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
 }
-printf('\n');
-return (0);
-}
+
