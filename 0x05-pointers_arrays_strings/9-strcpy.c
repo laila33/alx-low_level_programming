5fbd786dd761d27fe9cda05f848f4f7c4439fcaa
laila33 <lailaabdelnaser64@gmail.com>
2023-05-02 11:02:06 -0700

    x

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,11 @@
  */
 char *strc(char *dest, const char *src)
 {
-	int index = 0;
+	int i;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 	return (dest);
 }
