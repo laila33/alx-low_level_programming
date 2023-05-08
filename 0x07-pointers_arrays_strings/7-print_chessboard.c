26e73a726699355716d4bccc4a95183e62a6d0ee
laila33 <lailaabdelnaser64@gmail.com>
2023-05-08 14:03:06 -0700

    q

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[i][k]);
+		}
+		_putchar('\n');
+	}
+}
