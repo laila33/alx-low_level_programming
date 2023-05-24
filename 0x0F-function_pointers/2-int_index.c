4735b9b2413ba3f83bcdb3cb260eb99c67f0a04f
laila33 <lailaabdelnaser64@gmail.com>
2023-05-24 10:38:02 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
+
 	for (j = 0; j < size; j++)
 	{
-		if (cmp(array[j]));
+		if (cmp(array[j]))
 			return (j);
 	}
 	return (-1);
