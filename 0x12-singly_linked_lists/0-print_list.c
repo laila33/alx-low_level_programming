9f6c83ce00331887d5d564cd27cda5eafe26a366
laila33 <lailaabdelnaser64@gmail.com>
2023-06-01 13:32:24 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h)
+	while (h == NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		n++;
 		h = h->next;
 
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,1 @@
-global    main
-extern    printf
-main:
-	  mov   edi, format
-          call  printf
-	  xor   eax, eax
-	  mov 	eax, 0
-	  ret
-format: db 'Hello, Holberton',0xa, 0
+
