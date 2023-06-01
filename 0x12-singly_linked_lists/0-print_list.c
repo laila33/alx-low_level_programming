ac269034da0fbe0294560f5a6375e6bb220c9193
laila33 <lailaabdelnaser64@gmail.com>
2023-06-01 14:06:12 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,24 @@
 
 #include "lists.h"
 
+#include "lists.h"
+
+/**
+ * strlenn- returns the length of a string
+ * @x: string.
+ * Return: length of @x.
+ */
+int strlenn(char *x)
+{
+	int j = 0;
+
+	if (!x)
+		return (0);
+	while (*x++)
+		j++;
+	return (j);
+}
+
 /**
  * print_list - prints all the elements of a list_t
  *
@@ -12,20 +30,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t counter 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
-			printf([0], "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		printf("[%d] %s\n", strlenn(h->str), h->str ? h->str : "(nil");
 		h = h->next;
-		counter;
+		s++;
 	}
-	return (counter
+	return (i);
 }
