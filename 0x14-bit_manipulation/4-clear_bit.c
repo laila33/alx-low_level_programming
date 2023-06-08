340234abd609c0c98cb0f808152949e7f560dff8
laila33 <lailaabdelnaser64@gmail.com>
2023-06-08 03:19:23 -0700

    a

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	*n = (~(1UL << index) | *n);
+	*n = (~(1UL << index) & *n);
 
 	return (1);
 }
