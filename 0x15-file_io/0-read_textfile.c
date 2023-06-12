5bca6672d936744bea4d4e8cac60fba6a42e7d3c
laila33 <lailaabdelnaser64@gmail.com>
2023-06-12 14:10:33 -0700

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile- Read  file .
+ *
+ * @filename: text file .
+ *
+ * @letters: letters to be read.
+ *
+ * Return: number of bytes.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t word;
+	char  buf[buffer * 8];
+
+
+	if (filename == NULL || letters == NULL)
+		return (0);
+
+	f =  open(filename, O_RNONLY);
+	if (f == -1)
+		return (0);
+
+	word = read(f, &buf[0], letter);
+	word = write(STDOUT_FILENO, &buf[0], word);
+	close(f);
+
+
+	return (word);
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+#define buffer 1024
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endiif
