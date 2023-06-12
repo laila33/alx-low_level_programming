f39ce39548c9cabe38adb7d2252a395223f2616a
laila33 <lailaabdelnaser64@gmail.com>
2023-06-12 14:20:44 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,11 +19,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL || letters == NULL)
 		return (0);
 
-	f =  open(filename, O_RNONLY);
+	f =  open(filename, O_RDONLY);
 	if (f == -1)
 		return (0);
 
-	word = read(f, &buf[0], letter);
+	word = read(f, &buf[0], letters);
 	word = write(STDOUT_FILENO, &buf[0], word);
 	close(f);
 
